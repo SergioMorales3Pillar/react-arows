eb50015d0ac153424c161278f98109868078b3bd
Sergio M <sergio.morales@3pillarglobal.com>
2022-01-27 09:56:33 -0600

    adding component

--- react-arrows/src/components/Arrows/Arrows.js
+++ react-arrows/src/components/Arrows/Arrows.js
@@ -1,77 +1,221 @@
+import React, { useEffect, useState } from "react";
+import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
+import Draggable from "react-draggable";
+import styles from "./Arrows.module.scss";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { Box } from "@mui/system";
 
-import React from 'react';
-import Xarrow, {useXarrow, Xwrapper} from 'react-xarrows';
-import Draggable from 'react-draggable';
-import styles from './Arrows.module.scss';
+const boxStyle = {
+  border: "grey solid 2px",
+  borderRadius: "10px",
+  padding: "5px",
+  height: "100px",
+  top: 250,
+  right: 50,
+};
+const boxStyleDashed = {
+  border: "grey solid 3px",
+  borderRadius: "10px",
+  padding: "5px",
+  top: 250,
+  right: 150,
+};
+const boxStyleDashed2 = {
+  border: "grey solid 3px",
+  borderRadius: "10px",
+  padding: "5px",
+  top: 250,
+  right: 250,
+};
+const boxStyleDashed3 = {
+  border: "grey solid 3px",
+  borderRadius: "10px",
+  padding: "5px",
+  top: 250,
+  right: 350,
+};
+const Item = styled(Box)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
-const boxStyle = {border: 'grey solid 2px', borderRadius: '10px', padding: '5px'};
-const boxStyleDashed = {border: 'grey solid 3px', borderRadius: '10px', padding: '5px'};
+const DraggableBox = ({ id }) => {
+  const updateXarrow = useXarrow();
 
-const DraggableBox = ({id}) => {
-    const updateXarrow = useXarrow();
-
-    const getStyle=()=>{
-      let returnStyle=boxStyle;
-      switch (id) {
-        case 'elem1':
-          returnStyle= boxStyleDashed;
-          break;
-          case 'elem2':
-            returnStyle= boxStyleDashed;
-            break;
-            case 'elem3':
-              returnStyle= boxStyleDashed;
-              break;  
-        default:
-          break;
-
-         
-      }
-      return returnStyle;
+  const getStyle = () => {
+    let returnStyle = boxStyle;
+    switch (id) {
+      case "elem9":
+        returnStyle = boxStyleDashed;
+        break;
+      case "elem10":
+        returnStyle = boxStyleDashed2;
+        break;
+      case "elem11":
+        returnStyle = boxStyleDashed3;
+        break;
+      default:
+        break;
     }
-    return (
-        <Draggable  start={{  position: {
+    return returnStyle;
+  };
+  return (
+    <Draggable
+      start={{
+        position: {
           x: 500,
-          y: 250
-        }}}
-        onClick={console.log("here")}  onDrag={updateXarrow} onStop={updateXarrow}>
-
-            <div id={id} style={getStyle()}>
-              {console.log(id)}
-                {id}
-            </div>
-        </Draggable>
-    );
+          y: 250,
+        },
+      }}
+      onClick={console.log("here")}
+      onDrag={updateXarrow}
+      onStop={updateXarrow}
+    >
+      <div id={id} style={getStyle()}>
+        {console.log(id)}
+        {id}
+      </div>
+    </Draggable>
+  );
 };
 
+const BadgeComponent = ({ id }) => {
+  const updateXarrow = useXarrow();
+  const [open, setOpen] = useState(false);
+
+  const getStyle = () => {
+    let returnStyle = boxStyle;
+    switch (id) {
+      case "elem1":
+        returnStyle = boxStyleDashed;
+        break;
+      case "elem2":
+        returnStyle = boxStyleDashed;
+        break;
+      case "elem3":
+        returnStyle = boxStyleDashed;
+        break;
+      default:
+        break;
+    }
+    return returnStyle;
+  };
+
+  return (
+    <div style={boxStyle} onDrag={updateXarrow} onStop={updateXarrow}>
+      
+      <div 
+      style={{ minHeight:150}} 
+      id={id} onClick={e=>setOpen(!open)}>
+    1    {id}
+
+    <div style={{display:!open?"block":"none" ,  }}onClick={e=>setOpen(!open)}>
+     
+     
+        {id}
+      </div>
+
+      <div style={{display:open?"block":"none" , backgroundColor:'rgba(0, 0, 0, 0.8)'}} onClick={e=>setOpen(!open)}>
+      <img src="https://random.imagecdn.app/145/100"/>
+        <h1>Details for component</h1>
+        {id}
+      </div>
+
+
+      </div>
+
+    </div>
+  );
+};
 export default function Arrows() {
-    return (
-      <div className={styles.Arrows} data-testid="Arrows">
-        <div style={{display: 'flex', justifyContent: 'space-evenly', width: '100%'}}>
-            <Xwrapper>
-                <DraggableBox id={'elem1'}/>
-                <DraggableBox id={'elem2'}/>
-                <DraggableBox id={'elem3'}/>
-                <DraggableBox id={'elem4'}/>
-                <DraggableBox id={'elem5'}/>
-                <DraggableBox id={'elem6'}/>
-                <DraggableBox id={'elem7'}/>
-                <DraggableBox id={'elem8'}/>
+  return (
+    <div className={styles.Arrows} data-testid="Arrows">
+      <div
+        style={{
+          justifyContent: "space-evenly",
+          width: "100%",
+        }}
+      >
+        <Xwrapper>
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <Item>
+                <BadgeComponent id={"elem11"} />
+              </Item>
+            </Grid>
+          
+            <Grid item xs={4}>
+              <Item>
+                {" "}
+                <BadgeComponent id={"elem12"} />
+              </Item>
+            </Grid>
+        
+            <Grid item xs={4}>
+              <Item>
+              <BadgeComponent id={"elem13"} />
+              </Item>
+            </Grid>
+          </Grid>
+          
+          <br></br>    <br></br>    <br></br>
+          <Xarrow start={"elem11"} end="elem12" />
+          <Xarrow start={"elem12"} end="elem13" dashness={{ animation: 0 }} />
+          
+        <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <Item>
+                <BadgeComponent id={"elem21"} />
+              </Item>
+            </Grid>
+ 
+            <Grid item xs={4}>
+              <Item></Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>
+              <BadgeComponent id={"elem22"} />
+              </Item>
+            </Grid>
+          </Grid>
 
-                <Xarrow start={'elem1'} end="elem2"/>
-                <Xarrow start={'elem2'} end="elem3" dashness={{animation:0}}  />
+          <Xarrow start={"elem21"} end="elem12" />
+          <Xarrow start={"elem21"} end="elem22" dashness={{ animation: 0 }} />
+          <Xarrow start={"elem22"} end="elem13" dashness={{ animation: 0 }} />
+          
+      
+       
+          <br></br>    <br></br>    <br></br>
 
-                <Xarrow start={'elem3'} end="elem8" dashness={{animation:0}} />
+    
+        <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <Item>
+                <BadgeComponent id={"elem31"} />
+              </Item>
+            </Grid>
+          
+            <Grid item xs={4}>
+              <Item>
+              <BadgeComponent id={"elem32"} />
+              </Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>
+              <BadgeComponent id={"elem33"} />
+              </Item>
+            </Grid>
+          </Grid>
 
-                <Xarrow start={'elem4'} end="elem5" />
-                <Xarrow start={'elem5'} end="elem6"  />
-                <Xarrow start={'elem6'} end="elem7" dashness={{animation:1}} />
-                <Xarrow start={'elem7'} end="elem8" dashness={{animation:0}} />
-                
-            
-            </Xwrapper>
-        </div>
-        </div>
-
-    );
+          <Xarrow start={"elem31"} end="elem32" />
+          <Xarrow start={"elem32"} end="elem22" />
+      
+        </Xwrapper>
+      </div>
+    </div>
+  );
 }
